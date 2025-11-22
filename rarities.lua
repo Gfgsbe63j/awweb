bb74ce4e6ded36007c29eb2255efc8aab9fb6669
Gfgsbe63j <141261453+Gfgsbe63j@users.noreply.github.com>
2025-11-22 14:07:12 +0200

    Create rarities.lua

---
+++ rarities.lua
@@ -0,0 +1,60 @@
+-- Saved by UniversalSynSaveInstance (Join to Copy Games) https://discord.gg/wx4ThpAsmw
+
+-- Decompiled with Medal in Seliware
+
+local v1 = {
+    ["Common"] = {
+        ["Display"] = "COMMON",
+        ["Order"] = 1,
+        ["StarScale"] = 1,
+        ["StarMaxLevelScale"] = 0.8,
+        ["LuckWeight"] = 1
+    },
+    ["Uncommon"] = {
+        ["Display"] = "UNCOMMON",
+        ["Order"] = 2,
+        ["StarScale"] = 1.1,
+        ["StarMaxLevelScale"] = 0.9,
+        ["LuckWeight"] = 1.5
+    },
+    ["Rare"] = {
+        ["Display"] = "RARE",
+        ["Order"] = 3,
+        ["StarScale"] = 1.25,
+        ["StarMaxLevelScale"] = 1,
+        ["LuckWeight"] = 2
+    },
+    ["Epic"] = {
+        ["Display"] = "EPIC",
+        ["Order"] = 4,
+        ["StarScale"] = 1.4,
+        ["StarMaxLevelScale"] = 1.4,
+        ["LuckWeight"] = 2.5
+    },
+    ["Legend"] = {
+        ["Display"] = "LEGEND",
+        ["Order"] = 5,
+        ["StarScale"] = 1.6,
+        ["StarMaxLevelScale"] = 1.9,
+        ["LuckWeight"] = 3
+    },
+    ["Mythic"] = {
+        ["Display"] = "MYTHIC",
+        ["Order"] = 6,
+        ["StarScale"] = 1.8,
+        ["StarMaxLevelScale"] = 2.4,
+        ["LuckWeight"] = 3.5
+    },
+    ["Secret"] = {
+        ["Display"] = "SECRET",
+        ["Order"] = 7,
+        ["StarScale"] = 2.5,
+        ["StarMaxLevelScale"] = 3,
+        ["LuckWeight"] = 4
+    },
+    ["Exclusive"] = {
+        ["Display"] = "EXCLUSIVE",
+        ["Order"] = 8
+    }
+}
+return v1
